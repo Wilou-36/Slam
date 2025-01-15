716787af2f46f10350537f62150f7708f841530b
Wilou-36 <williams.drg36@gmail.com>
2025-01-15 11:55:30 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using TP1;
-
-chauffeur navigo = new chauffeur("pannini", "bobby", 1233963, true, "aucune");
-
-navigo.AfficherInfos();
-
-try
-{
-    navigo.Assignermission("paname");
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.Message);
-}
-
-
-
+Console.WriteLine("Hello, World!");
